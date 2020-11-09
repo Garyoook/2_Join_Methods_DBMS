b7e28a5019a7d9427a1c4f0977acc65fb2a582c9
Gary Gao <gykawesome@163.com>
2020-11-09 18:15:40 +0000

    Modified ShapeCount.c

--- Source/ShapeCount.c
+++ Source/ShapeCount.c
@@ -1,26 +1,83 @@
-typedef void* SortMergeJoinDatabase;
+#include <stddef.h>
+
+typedef void *SortMergeJoinDatabase;
+
 SortMergeJoinDatabase SortMergeJoinAllocateDatabase(unsigned long totalNumberOfEdgesInTheEnd);
+
 void SortMergeJoinInsertEdge(SortMergeJoinDatabase database, int fromNodeID, int toNodeID,
-                             int edgeLabel);
+                             int edgeLabel) {
+    // TODO: finish this function.
+}
+
 int SortMergeJoinRunQuery(SortMergeJoinDatabase database, int edgeLabel1, int edgeLabel2,
-                          int edgeLabel3);
+                          int edgeLabel3) {
+    // TODO: finish this function.
+
+    return 0;
+}
+
 void SortMergeJoinDeleteEdge(SortMergeJoinDatabase database, int fromNodeID, int toNodeID,
-                             int edgeLabel);
-void SortMergeJoinDeleteDatabase(SortMergeJoinDatabase database);
+                             int edgeLabel) {
+    // TODO: finish this function.
 
-typedef void* HashjoinDatabase;
-HashjoinDatabase HashjoinAllocateDatabase(unsigned long totalNumberOfEdgesInTheEnd);
-void HashjoinInsertEdge(HashjoinDatabase database, int fromNodeID, int toNodeID, int edgeLabel);
-int HashjoinRunQuery(HashjoinDatabase database, int edgeLabel1, int edgeLabel2, int edgeLabel3);
-void HashjoinDeleteEdge(HashjoinDatabase database, int fromNodeID, int toNodeID, int edgeLabel);
-void HashjoinDeleteDatabase(HashjoinDatabase database);
+}
 
-typedef void* CompetitionDatabase;
+void SortMergeJoinDeleteDatabase(SortMergeJoinDatabase database) {
+    // TODO: finish this function.
+
+}
+
+typedef void *HashjoinDatabase;
+
+HashjoinDatabase HashjoinAllocateDatabase(unsigned long totalNumberOfEdgesInTheEnd) {
+    // TODO: finish this function.
+    return NULL;
+}
+
+void HashjoinInsertEdge(HashjoinDatabase database, int fromNodeID, int toNodeID, int edgeLabel) {
+    // TODO: finish this function.
+
+}
+
+int HashjoinRunQuery(HashjoinDatabase database, int edgeLabel1, int edgeLabel2, int edgeLabel3) {
+    // TODO: finish this function.
+
+    return 0;
+}
+
+void HashjoinDeleteEdge(HashjoinDatabase database, int fromNodeID, int toNodeID, int edgeLabel) {
+    // TODO: finish this function.
+
+}
+
+void HashjoinDeleteDatabase(HashjoinDatabase database) {
+    // TODO: finish this function.
+
+}
+
+typedef void *CompetitionDatabase;
+
 CompetitionDatabase CompetitionAllocateDatabase(unsigned long totalNumberOfEdgesInTheEnd);
+
 void CompetitionInsertEdge(CompetitionDatabase database, int fromNodeID, int toNodeID,
-                           int edgeLabel);
+                           int edgeLabel) {
+    // TODO: finish this function.
+
+}
+
 int CompetitionRunQuery(CompetitionDatabase database, int edgeLabel1, int edgeLabel2,
-                        int edgeLabel3);
+                        int edgeLabel3) {
+    // TODO: finish this function.
+    return 0;
+}
+
 void CompetitionDeleteEdge(CompetitionDatabase database, int fromNodeID, int toNodeID,
-                           int edgeLabel);
-void CompetitionDeleteDatabase(CompetitionDatabase database);
+                           int edgeLabel) {
+    // TODO: finish this function.
+
+}
+
+void CompetitionDeleteDatabase(CompetitionDatabase database) {
+    // TODO: finish this function.
+
+}
