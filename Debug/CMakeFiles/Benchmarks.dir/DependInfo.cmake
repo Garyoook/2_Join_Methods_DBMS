cb1101df67c3328e2ff59493d6a3af57792f9c01
Gary Gao <gykawesome@163.com>
2020-11-09 17:42:46 +0000

    setting up Debug/ and Release/ directories

---
+++ Debug/CMakeFiles/Benchmarks.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yg9418/Desktop/Year3/Advanced_Databases/shapecount_72/Benchmarks/ShapeCountBenchmarks.cpp" "/Users/yg9418/Desktop/Year3/Advanced_Databases/shapecount_72/Debug/CMakeFiles/Benchmarks.dir/Benchmarks/ShapeCountBenchmarks.cpp.o"
+  "/Users/yg9418/Desktop/Year3/Advanced_Databases/shapecount_72/Dependencies/Edge.cpp" "/Users/yg9418/Desktop/Year3/Advanced_Databases/shapecount_72/Debug/CMakeFiles/Benchmarks.dir/Dependencies/Edge.cpp.o"
+  "/Users/yg9418/Desktop/Year3/Advanced_Databases/shapecount_72/Dependencies/GraphGen_notSorted.cpp" "/Users/yg9418/Desktop/Year3/Advanced_Databases/shapecount_72/Debug/CMakeFiles/Benchmarks.dir/Dependencies/GraphGen_notSorted.cpp.o"
+  "/Users/yg9418/Desktop/Year3/Advanced_Databases/shapecount_72/Dependencies/GraphGen_sorted.cpp" "/Users/yg9418/Desktop/Year3/Advanced_Databases/shapecount_72/Debug/CMakeFiles/Benchmarks.dir/Dependencies/GraphGen_sorted.cpp.o"
+  "/Users/yg9418/Desktop/Year3/Advanced_Databases/shapecount_72/Dependencies/PaRMAT_vector.cpp" "/Users/yg9418/Desktop/Year3/Advanced_Databases/shapecount_72/Debug/CMakeFiles/Benchmarks.dir/Dependencies/PaRMAT_vector.cpp.o"
+  "/Users/yg9418/Desktop/Year3/Advanced_Databases/shapecount_72/Dependencies/Square.cpp" "/Users/yg9418/Desktop/Year3/Advanced_Databases/shapecount_72/Debug/CMakeFiles/Benchmarks.dir/Dependencies/Square.cpp.o"
+  "/Users/yg9418/Desktop/Year3/Advanced_Databases/shapecount_72/Dependencies/utils.cpp" "/Users/yg9418/Desktop/Year3/Advanced_Databases/shapecount_72/Debug/CMakeFiles/Benchmarks.dir/Dependencies/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "deps/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Release/googlebenchmark-prefix/src/googlebenchmark-build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/yg9418/Desktop/Year3/Advanced_Databases/shapecount_72/Release/googlebenchmark-prefix/src/googlebenchmark
+# Build directory: /Users/yg9418/Desktop/Year3/Advanced_Databases/shapecount_72/Release/googlebenchmark-prefix/src/googlebenchmark-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("test")
